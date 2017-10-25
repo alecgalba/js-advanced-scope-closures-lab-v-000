ded59a5013d48caaefeac7e89493b05186b2395b
alecgalba <alecgalba@gmail.com>
2017-10-25 11:47:32 -0400

    Vale

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(blockRange) {
+  return function(a, b) {
+    let distance = parseInt(b) - parseInt(a);
+    if (distance > blockRange) {
+      return `${distance - blockRange} blocks out of range`;
+    } else {
+      return `within range by ${blockRange - distance}`;
+    }
+  }
+}
+
+function produceTipCalculator(tip) {
+  return function(amount) {
+    return tip * amount;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
